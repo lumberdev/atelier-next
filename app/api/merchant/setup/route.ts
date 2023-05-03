e2d4e3aca94ecdb39da110b3fc08ee37fa77d21e
Luis Fernando Saavedra Meza <fernando@fsvdr.me>
2023-05-02 19:39:09 -0600

    feat: Add CORS support

--- app/api/merchant/setup/route.ts
+++ app/api/merchant/setup/route.ts
@@ -1,6 +1,16 @@
 import { prisma } from '@lib/prisma';
 import { NextResponse } from 'next/server';
 
+export const OPTIONS = async (request: Request) => {
+  return new Response('', {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+    },
+  });
+};
+
 export const POST = async (request: Request) => {
   const { identifier, name, password } = (await request.json()) as {
     identifier: string;
@@ -22,5 +32,7 @@ export const POST = async (request: Request) => {
     },
   });
 
-  return new NextResponse(JSON.stringify({ success: true }), { status: 201 });
+  return new NextResponse(JSON.stringify({ success: true }), {
+    status: 201,
+  });
 };
